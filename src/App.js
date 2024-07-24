85a470926dfea2c2a4f2e5d149d79003b4d1bbec
Abstract Programmer <AbstractProgrammer@example.com>
2024-07-25 02:10:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [result, setResult] = useState("0")
+
+  const setnewValue = (value) => {
+    setResult(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form>
+         <input className="input" type='text' value={result}></input>
+      </form>
+     <div className='buttons'>
+      <button onClick={() => setnewValue('7')}>7</button>
+      <button onClick={() => setnewValue('8')}>8</button>
+      <button onClick={() => setnewValue('9')}>9</button>
+      <button onClick={() => setnewValue('4')}>4</button>
+      <button onClick={() => setnewValue('5')}>5</button>
+      <button onClick={() => setnewValue('6')}>6</button>
+      <button onClick={() => setnewValue('1')}>1</button>
+      <button onClick={() => setnewValue('2')}>2</button>
+      <button onClick={() => setnewValue('3')}>3</button>
+
     </div>
+
+    </div>
+
+    
   );
 }
 
